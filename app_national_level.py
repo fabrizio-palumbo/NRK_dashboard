c725cae528c53a99a6579050ccce4d490cd35ae2
fabrizio-palumbo <fabrizio@oslomet.no>
2022-11-07 18:14:29 +0100

    fixed group by

--- app_national_level.py
+++ app_national_level.py
@@ -77,14 +77,14 @@ data_kpr = pd.read_csv(db_folder+ 'kpr.csv',encoding='utf-8')
 data_all_ncr = pd.read_csv(db_folder+ 'all_ncr.csv',encoding='latin-1',index_col=0)
 data_all_ncr=data_all_ncr.apply(pd.to_numeric, errors='coerce')
 # data_all_ncr["komnr"]=data_all_ncr["komnr"].astype(str)
-data_all_ncr=data_all_ncr.groupby(by=['komnr'], axis=0, level=None, as_index=True, sort=False,dropna=True).sum()
+data_all_ncr=data_all_ncr.groupby(by=['komnr'], axis=0, level=None, as_index=True, sort=False,dropna=True).sum(min_count=1)
 
 #data_all_ncr.set_index('komnr',drop=True, append=False, inplace=True, verify_integrity=True)
 
 data_med_ncr = pd.read_csv(db_folder+ 'med_NCR.csv',encoding='latin-1',index_col=0)
 data_med_ncr=data_med_ncr.apply(pd.to_numeric, errors='coerce')
 # data_med_ncr["komnr"]=data_med_ncr["komnr"].astype(str)
-data_med_ncr=data_med_ncr.groupby(by=['komnr'], axis=0, level=None, as_index=True, sort=False,dropna=True).sum()#.set_index('komnr',drop=True, append=False, inplace=True, verify_integrity=True)
+data_med_ncr=data_med_ncr.groupby(by=['komnr'], axis=0, level=None, as_index=True, sort=False,dropna=True).sum(min_count=1)#.set_index('komnr',drop=True, append=False, inplace=True, verify_integrity=True)
 
 jsonMap_of_norway=open(db_folder+ "kommuner2021.json");
 norwayMap= loadMap(jsonMap_of_norway)
@@ -176,6 +176,7 @@ def stat_test(df):
     return
 
 def main():
+    st.write(data_med_ncr)
     ncr_visualization = st.checkbox('Visualize ncr data')
     if ncr_visualization:
         ncr_all_violin=plt.figure()
